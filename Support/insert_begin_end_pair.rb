9cdbff61d94daaa85b58c4b5fcb004d1bae61541
Brad Miller <bonelake@gmail.com>
2005-11-02 19:14:10 +0000

    I Forgot I had a copy of ruby in a nutshell lying around.  OK.  Now the Insert Begin/End pair macro will optionally add a label{xxx:} if the variable TM_LATEX_INSERT_LABEL  is set to 1.  If the  variable is not set or set to something else then no label will be added.
    I made it slightly smart about what the label should be called but I imagine we can improve this  over time. 
    
    git-svn-id: http://svn.textmate.org/trunk/Bundles/Latex.tmbundle@2080 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/insert_begin_end_pair.rb
+++ Support/insert_begin_end_pair.rb
@@ -4,6 +4,7 @@ commands = /^cite|footnote|label|ref$/
 shortcuts = { "it" => "textit", "bf" => "textbf" }
 
 sel = ENV['TM_SELECTED_TEXT'].to_s
+lab = ENV['TM_LATEX_INSERT_LABEL'].to_i
 if(sel.strip != '')
 	name = sel.strip[/^\S+/]
   if(commands.match(name))
@@ -11,7 +12,12 @@ if(sel.strip != '')
   elsif !shortcuts[name].nil?
     print("\\#{shortcuts[name]}{$1}")
   else
-    print("\\begin{#{name}}\n\t$1\n\\end{#{name}}")
+    if (lab == 1) 
+      labPrefix = name.slice(0,3)
+      print("\\begin{#{name}}\n\\label{#{labPrefix}:}\n\t$1\n\\end{#{name}}")
+    else
+      print("\\begin{#{name}}\n\t$1\n\\end{#{name}}")
+    end
   end
 else
 	print("#{sel}\\begin{${1:name}}\n#{sel}\t$2\n#{sel}\\end{$1}")
