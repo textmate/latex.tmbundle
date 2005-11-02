140fff0c152afa470ebec2fa6ce1ba7855903872
Allan Odgaard <git@abetarda.com>
2005-11-02 14:03:05 +0000

    • improved so that typing e.g. ref + ^> will expand to \ref{…} and it + ^> expands to \textit{…}
    There are two variables (commands and shortcuts), feel free to add known commands and shortcuts (it, bf, tt, etc. being shortcuts for text*)
    
    git-svn-id: http://svn.textmate.org/trunk/Bundles/Latex.tmbundle@2069 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/insert_begin_end_pair.rb
+++ Support/insert_begin_end_pair.rb
@@ -1,9 +1,18 @@
 #!/usr/bin/env ruby
 
+commands = /^cite|footnote|label|ref$/
+shortcuts = { "it" => "textit", "bf" => "textbf" }
+
 sel = ENV['TM_SELECTED_TEXT']
 if(sel.to_s.strip != '')
 	name = sel.strip[/^\S+/]
-	print("\\begin{#{name}}\n\t$1\n\\end{#{name}}")
+  if(commands.match(name))
+    print("\\#{name}{$1}")
+  elsif !shortcuts[name].nil?
+    print("\\#{shortcuts[name]}{$1}")
+  else
+    print("\\begin{#{name}}\n\t$1\n\\end{#{name}}")
+  end
 else
 	print("#{sel}\\begin{${1:name}}\n#{sel}\t$2\n#{sel}\\end{$1}")
 end
