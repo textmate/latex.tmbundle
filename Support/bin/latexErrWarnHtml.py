8b9e882b3951b652060d3cfea678c1bb6d826064
Brad Miller <bonelake@gmail.com>
2007-07-15 19:54:09 +0000

    fix up a couple minor things from previous commit.  latexmk.pl sends some output to stderr which was getting prepended to the output window unformatted. Also replaced some misc stdin readline calls with tex.readline() to conform to the new method of having latexErrWarn run the typesetting program and capturing the output from a pipe. Update the script to match the output from the newer version of latexmk.pl Finally make the script smarter about detecting whether file:line:error style messages are being used.  If not, then output a warning,  and try to be smarter about matching error lines that start with a !
    git-svn-id: http://svn.textmate.org/trunk/Bundles/Latex.tmbundle@7743 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/bin/latexErrWarnHtml.py
+++ Support/bin/latexErrWarnHtml.py
@@ -18,6 +18,7 @@ warnPat = re.compile('LaTeX Warning.*?input line (\d+).$')
 errPat = re.compile('^([\.\/\w\x7f-\xff]+\.tex):(\d+):.*')
 incPat = re.compile('.*\<use (.*?)\>');
 miscWarnPat = re.compile('LaTeX Warning:.*')
+fileLinePat = re.compile('^\s+file:line:error style messages enabled')
 
 verbose = False
 if len(sys.argv) > 1 and sys.argv[1] == '-v':
@@ -27,51 +28,104 @@ if len(sys.argv) == 3:
     texCommand = sys.argv[1]
     fileName = sys.argv[2]
 else:
-    sys.stderr.write("Usage: "+argv[0]+" [-v] tex-command file.tex")
+    sys.stderr.write("Usage: "+sys.argv[0]+" [-v] tex-command file.tex")
     sys.exit(255)
 
-tex = os.popen(texCommand+" "+fileName)
+texin,tex = os.popen4(texCommand+" "+fileName)
 
 numWarns = 0
 numErrs = 0
 numRuns = 0
+numLines=0
 inbibidx = False
 isFatal = False
+fileLineErrors = False
+usingLatexmk = False
+
+# for some reason the coloring for error and warning only seem to work in the default style.
+# The following lines fix that.  The bibtex style works OK for light colored styles but not
+# for dark, I'm not sure how to handle that at the moment.
+print """<style>
+.warning {
+    color: orange;
+}
+a.warning {
+    color: orange;
+}
+.bibtex {
+    background-color: #CBF4EF;
+}
+.error {
+    color: red;
+}
+a.error {
+    color: red;
+}
+</style>
+"""
 
 print '<pre>'
 line = tex.readline()
 while line:
     line = line.rstrip("\n")
+    numLines += 1
     # print out first line
     if re.match('^This is',line):
-        print line[:-1]
+        print line
     if re.match('^Document Class',line):
-        print line[:-1]
+        print line
     m = newFilePat.match(line)
     if m:
         currentFile = m.group(1)
         print "<h3>Typesetting: " + currentFile + "</h3>"
+        if inbibidx:
+            inbibidx=False
+            print '</div>'
+        
+    if re.match('^Latexmk',line):
+        usingLatexmk = True
+        print line
+
+    fl = fileLinePat.match(line)
+    if fl:
+        fileLineErrors = True
+
+    # if we are not using latexmk and we have processed the first 10 lines without detecting a file line error style
+    # message then spit out a warning that we may be missing error messages.
+    if fileLineErrors == False and usingLatexmk == False and numLines == 10:
+        print "TextMate Warning:  You may be missing error and warning messages.  file:line:error style messages must be enabled"
+        
     inf = incPat.match(line)
     if inf:
         print "    Including: " + inf.group(1)
     if re.match('^Output written',line):
-        print line[:-1]
-    if re.match('Running makeindex',line):
+        print line
+    if re.match("Running 'makeindex",line):
         print '<div class="mkindex">'        
         print '<h3>' + line[:-1] + '</h3>'
-        sys.stdin.readline()
+        tex.readline()
         inbibidx = True
     
-    if re.match('\! LaTeX Error:', line):
-        print '<div class="error">'
-        print line
-        print '</div>'
-        numErrs = numErrs + 1
+    # If file line error style messages are off then do our best to detect an error or warning
+    # apparently these lines all start with a ! but do not necessarily contain the word error or warning
+    # beamer is one example...
+    if re.match('\!.*',line):
+        if re.match('\! LaTeX Error:', line):
+            print '<div class="error">'
+            print line
+            print '</div>'
+            numErrs = numErrs + 1
+        else:
+            print '<div class="warning">'
+            print line
+            print '</div>'
+            numWarns += 1
+
     if re.match('^Error: pdflatex', line):
         numErrs = numErrs + 1
         print '<div class="error">'
         print line
-        line = sys.stdin.readline()
+        line = tex.readline()
         if line and re.match('^ ==> Fatal error occurred', line):
             print line.rstrip("\n")
             print '</div>'
@@ -84,10 +138,10 @@ while line:
     if le:
         print '<div class="warning">'
         latexErrorMsg = '<a class="warning" href="' + make_link(os.getcwd()+'/'+le.group(1),le.group(2)) +  '">' + le.group(1)+":"+le.group(2) + '</a> '+le.group(3)
-        line = sys.stdin.readline()
+        line = tex.readline()
         while len(line) > 1:
             latexErrorMsg = latexErrorMsg+line
-            line = sys.stdin.readline()
+            line = tex.readline()
         print latexErrorMsg+'</div>'
         numWarns = numWarns + 1
     
@@ -105,10 +159,10 @@ while line:
         print '<a class="error" href="' + make_link(os.getcwd()+'/'+ts.group(1),'1') +  '">' + ts.group(1) + '</a>'
         print '</div>'
         
-    if re.match('Running bibtex',line):
+    if re.match("Running 'bibtex",line):
         print '<div class="bibtex">'
         print '<h3>' + line[:-1] + '</h3>'
-        sys.stdin.readline()
+        tex.readline()
         inbibidx = True
         
     if re.match('---',line) and inbibidx:
@@ -117,11 +171,12 @@ while line:
 
     if re.match("Warning--I didn't find a database entry",line):
         print line
-        
+    
     if re.match('Run number',line):
         print '<hr />'
         numWarns = 0
         numErrs = 0
+        numLines = 0
         numRuns = numRuns + 1
         print '<hr />'
         
@@ -137,14 +192,14 @@ while line:
         numWarns = numWarns+1
     elif e:
         numErrs = numErrs+1
-        nextLine = sys.stdin.readline()
+        nextLine = tex.readline()
         print '<a class="error" href="' + make_link(os.getcwd()+e.group(1)[1:], e.group(2)) + '">'+line[:-1]+nextLine+"</a>"        
     elif me:
         numWarns = numWarns + 1
         sys.stdout.write('<p class="warning">' + line[:-1] + '</p>')
     else:
         if verbose:
-            print line[:-1]
+            print line
     line = tex.readline()
 texStatus = tex.close()
 eCode = 0
