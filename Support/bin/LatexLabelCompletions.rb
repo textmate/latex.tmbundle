8d1e84159172066dfebefd9c270dcdbb714bc5ee
Allan Odgaard <git@abetarda.com>
2013-07-29 18:20:51 +0200

    Fix label completion (⎋)
    This command used stdin to get the word being completed, but should use the TM_CURRENT_WORD variable.
    
    Additionally it used grep with a string which should be a “begin of buffer” anchored regular expression (based on the phrase).

--- Support/bin/LatexLabelCompletions.rb
+++ Support/bin/LatexLabelCompletions.rb
@@ -1,8 +1,8 @@
 #!/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/ruby
 require ENV["TM_BUNDLE_SUPPORT"] + "/lib/LaTeXUtils.rb"
-phrase = STDIN.read.chomp
+phrase = ENV['TM_CURRENT_WORD']
 include LaTeX
 items = LaTeX.get_labels
-items = items.grep(phrase) if phrase != ""
+items = items.grep(/^#{Regexp.escape(phrase)}/) if phrase != ""
 exit if items.empty?
 puts items.join("\n")
