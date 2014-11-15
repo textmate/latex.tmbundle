e595f99181f09c44beff39bca1210bb4209ec9c9
René Schwaiger <sanssecours@f-m.fm>
2014-11-15 18:21:00 +0100

    Format `tmprefs` according to `PEP 8`
    #ignore

--- Support/bin/tmprefs.py
+++ Support/bin/tmprefs.py
@@ -5,34 +5,36 @@ from Foundation import NSDictionary
 from subprocess import PIPE, STDOUT, Popen
 
 # The preference file for textmate to retrieve the prefs from
-if os.environ.has_key('TM_APP_IDENTIFIER'):
+if 'TM_APP_IDENTIFIER' in os.environ:
     TM_PREFERENCE_FILE = os.environ['TM_APP_IDENTIFIER'] + '.plist'
 else:
     TM_PREFERENCE_FILE = 'com.macromates.textmate.plist'
+
 
 class Preferences(object):
     """docstring for Preferences"""
     def __init__(self):
         super(Preferences, self).__init__()
         self.defaults = {
-            'latexAutoView' : 1,
-            'latexEngine' : "pdflatex",
-            'latexEngineOptions' : "",
-            'latexVerbose' : 0,
-            'latexUselatexmk' : 0,
-            'latexViewer' : "TextMate",
-            'latexKeepLogWin' : 1,
-            'latexDebug' : 0,
+            'latexAutoView': 1,
+            'latexEngine': "pdflatex",
+            'latexEngineOptions': "",
+            'latexVerbose': 0,
+            'latexUselatexmk': 0,
+            'latexViewer': "TextMate",
+            'latexKeepLogWin': 1,
+            'latexDebug': 0,
         }
         self.prefs = self.defaults.copy()
         self.prefs.update(self.readTMPrefs())
 
-    def __getitem__(self,key):
+    def __getitem__(self, key):
         """docstring for __getitem__"""
-        return self.prefs.get(key,None)
+        return self.prefs.get(key, None)
 
     def readTMPrefs(self):
-        """readTMPrefs reads the textmate preferences file and constructs a python dictionary.
+        """readTMPrefs reads the textmate preferences file and constructs a
+        python dictionary.
         The keys that are important for latex are as follows:
         latexAutoView = 0
         latexEngine = pdflatex
@@ -40,16 +42,18 @@ class Preferences(object):
         latexUselatexmk = 0
         latexViewer = Skim
         """
-        return NSDictionary.dictionaryWithContentsOfFile_(os.environ["HOME"]+"/Library/Preferences/" + TM_PREFERENCE_FILE)
+        return NSDictionary.dictionaryWithContentsOfFile_(
+            os.environ["HOME"]+"/Library/Preferences/" + TM_PREFERENCE_FILE)
 
     def toDefString(self):
         """docstring for toDefString"""
         instr = plistlib.writePlistToString(self.defaults)
-        runObj = Popen('pl',shell=True,stdout=PIPE,stdin=PIPE,stderr=STDOUT,close_fds=True)
+        runObj = Popen('pl', shell=True, stdout=PIPE, stdin=PIPE,
+                       stderr=STDOUT, close_fds=True)
         runObj.stdin.write(instr)
         runObj.stdin.close()
         defstr = runObj.stdout.read()
-        return defstr.replace("\n","")
+        return defstr.replace("\n", "")
 
 if __name__ == '__main__':
     test = Preferences()
@@ -58,5 +62,3 @@ if __name__ == '__main__':
     print test['Foo']
     useLatexMk = test['latexUselatexmk']
     print useLatexMk
-
-
