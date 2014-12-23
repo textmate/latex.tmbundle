1d205424e9d7ba6f0e2d6ce569b55fd985d8bbd6
René Schwaiger <sanssecours@f-m.fm>
2014-12-23 11:04:59 +0100

    Format code for “Reformat” (Table)
    #ignore

--- Support/bin/format_table.rb
+++ Support/bin/format_table.rb
@@ -4,28 +4,30 @@ def reformat(table_content)
   lines = table_content
   s = lines.slice!(/^.*?\}\s*\n/)
   # Place any \hline's not on a line of their own in their own line
-  lines.gsub!(/(\\hline\s*)(?!\n)/,"\\hline\\\\\\\\")
+  lines.gsub!(/(\\hline\s*)(?!\n)/, '\\hline\\\\\\\\')
   lines = lines.split(/\\\\/)
   data = lines.map do |line|
-    line.split(/&/).map{|i| i.strip}
+    line.split(/&/).map { |i| i.strip }
   end
-  cols = data.map{|i| i.length}.max
+  cols = data.map { |i| i.length }.max
   widths = []
   cols.times do |i|
-    widths << data.inject(0) do |maximum,line| if line.length <= i then maximum else [maximum,line[i].length].max end end
+    widths << data.inject(0) do |maximum, line|
+      if line.length <= i then maximum else [maximum, line[i].length].max end
+    end
   end
-  pattern = widths.map{|i| "%#{i}s"}.join(" & ")
+  pattern = widths.map { |i| "%#{i}s" }.join(' & ')
   print s.chomp
-  prev=false
+  prev = false
   for line in data do
     print(prev ? "\\\\\n" : "\n")
     if line.length <= 1 then
       print line
-      prev=false
+      prev = false
     else
-      line.fill("",(line.length+1)..cols)
-      printf(pattern,*line)
-      prev=true
+      line.fill('', (line.length + 1)..cols)
+      printf(pattern, *line)
+      prev = true
     end
   end
   print "\n"
