538983483cf1d32887bf328ccd549b5311d91d8f
René Schwaiger <sanssecours@f-m.fm>
2015-01-20 10:01:48 +0100

    Restructure `texdoc`
    #ignore

--- Support/bin/texdoc.py
+++ Support/bin/texdoc.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+# -----------------------------------------------------------------------------
+# Date:    2015-01-20
+# Authors: Brad Miller
+#          René Schwaiger (sanssecours@f-m.fm)
+# Version: 1
+# -----------------------------------------------------------------------------
+
 """Display documentation for tex packages.
 
 This script is a hacked together set of heuristics to try and bring some
@@ -29,11 +38,16 @@ and opens it in Previewer.
 
 """
 
+
+# -- Imports ------------------------------------------------------------------
+
 from __future__ import absolute_import
 
 import os
 import cPickle
 import time
+
+from os.path import exists
 
 if __name__ == '__main__' and __package__ is None:
     from os import sys, path
@@ -43,11 +57,7 @@ from lib.tex import (find_tex_packages, find_tex_directives,
                      find_file_to_typeset)
 
 
-
-pathDict = {}
-descDict = {}
-headings = {}
-
+# -- Functions ----------------------------------------------------------------
 
 def findBestDoc(myDir):
     """findBestDoc
@@ -91,214 +101,222 @@ def makeDocList():
         docDict[key] = doc[:-1]
     return docDict
 
-# Part 1
-# Find all the packages included in this file or its inputs
-tsDirs = find_tex_directives(os.environ["TM_FILEPATH"])
-fileName, filePath = find_file_to_typeset(tsDirs)
-mList = find_tex_packages(fileName)
+# -- Main ---------------------------------------------------------------------
 
-home = os.environ["HOME"]
-docdbpath = home + "/Library/Caches/TextMate"
-docdbfile = docdbpath + "/latexdocindex"
-ninty_days_ago = time.time() - (90 * 86400)
-cachedIndex = False
+if __name__ == '__main__':
 
-if os.path.exists(docdbfile) and os.path.getmtime(docdbfile) > ninty_days_ago:
-    infile = open(docdbfile, 'rb')
-    path_desc_list = cPickle.load(infile)
-    pathDict = path_desc_list[0]
-    descDict = path_desc_list[1]
-    headings = path_desc_list[2]
-    cachedIndex = True
-else:
-    # Part 2
-    # Parse the texdoctk database of packages
-    texMFbase = os.environ["TM_LATEX_DOCBASE"]
-    docIndex = os.environ["TEXDOCTKDB"]
+    pathDict = {}
+    descDict = {}
+    headings = {}
 
-    docBase = texMFbase + "/"  # + "doc/"
-    if docBase[-5:].rfind('doc') < 0:
-        docBase = docBase + "doc/"
+    # Part 1
+    # Find all the packages included in this file or its inputs
+    tsDirs = find_tex_directives(os.environ["TM_FILEPATH"])
+    fileName, filePath = find_file_to_typeset(tsDirs)
+    mList = find_tex_packages(fileName)
 
-    catalogDir = os.environ["TM_LATEX_HELP_CATALOG"]
+    home = os.environ["HOME"]
+    docdbpath = home + "/Library/Caches/TextMate"
+    docdbfile = docdbpath + "/latexdocindex"
+    ninty_days_ago = time.time() - (90 * 86400)
+    cachedIndex = False
 
-    texdocs = os.environ["TMTEXDOCDIRS"].split(':')
-    myDict = {}
-    for p in texdocs:
-        key = p[p.rfind('/')+1:]
-        myDict[key] = p
-
-    docDict = makeDocList()
-
-    try:
-        docIndexFile = open(docIndex, 'r')
-    except:
-        docIndexFile = []
-    for line in docIndexFile:
-        if line[0] == "#":
-            continue
-        elif line[0] == "@":
-            currentHeading = line[1:].strip()
-            headings[currentHeading] = []
-        else:
-            try:
-                lineFields = line.split(';')
-                key = lineFields[0]
-                desc = lineFields[1]
-                path = lineFields[2]
-            except:
-                print "Error parsing line: ", line
-
-            headings[currentHeading].append(key)
-            if path.rfind('.sty') >= 0:
-                path = docBase + "tex/" + path
-            else:
-                path = docBase + path
-                if not os.path.exists(path):
-                    # sometimes texdoctk.dat is misleading...
-                    altkey = path[path.rfind("/")+1:path.rfind(".")]
-                    if key in docDict:
-                        path = docDict[key]
-                    elif altkey in docDict:
-                        path = docDict[altkey]
-                    else:
-                        if key in myDict:
-                            path = findBestDoc(myDict[key])
-
-            pathDict[key] = path.strip()
-            descDict[key] = desc.strip()
-
-    # Part 3
-    # supplement texdoctk index with the regular texdoc catalog
-    try:
-        catList = os.listdir(catalogDir)
-    except:
-        catList = []
-    for fname in catList:
-        key = fname[:fname.rfind('.html')]
-        if key not in pathDict:
-            pathDict[key] = catalogDir + '/' + fname
-            descDict[key] = key
-            if key in docDict:
-                pathDict[key] = docDict[key]
-
-    # Continue to supplement with searched for files
-    for p in docDict.keys()+myDict.keys():
-        if p not in pathDict:
-            if p in docDict:
-                pathDict[p] = docDict[p].strip()
-                descDict[p] = p
-            else:
-                if p in myDict:
-                    path = findBestDoc(myDict[p])
-                    pathDict[p] = path.strip()
-                    descDict[p] = p
-
-    try:
-        if not os.path.exists(docdbpath):
-            os.mkdir(docdbpath)
-        outfile = open(docdbfile, 'wb')
-        cPickle.dump([pathDict, descDict, headings], outfile)
-    except:
-        print "<p>Error: Could not cache documentation index</p>"
-
-# Part 4
-# if a word was selected then view the documentation for that word
-# using the best available version of the doc as determined above
-cwPackage = os.environ.get("TM_CURRENT_WORD", None)
-if cwPackage in pathDict:
-    os.system("viewDoc.sh " + pathDict[cwPackage])
-    sys.exit()
-
-# Part 5
-# Print out the results in html/javascript
-# The java script gives us the nifty expand collapse outline look
-print """
-<style type="text/css"><!--
-.save{
-   behavior:url(#default#savehistory);}
-a.dsphead{
-   text-decoration:none;
-   font-family: "Lucida Grand", sans-serif
-   font-size: 120%;
-   font-weight: bold;
-   margin-left:0.5em;}
-a.dsphead:hover{
-   text-decoration:underline;}
-.dspcont{
-   display:none;
-   text-decoration:none;
-   font-family: "Bitstream Vera Sans Mono", "Monaco", monospace;
-   margin: 0px 20px 0px 20px;}
-.dspcont a{
-    text-decoration: none;
-}
-.dspcont a:hover{
-    text-decoration:underline;
-}
-div#mypkg{
-   text-decoration:none;
-   font-family: "Bitstream Vera Sans Mono", "Monaco", monospace;
-}
-div#mypkg a{
-   text-decoration:none;
-   font-family: "Bitstream Vera Sans Mono", "Monaco", monospace;
-}
-div#mypkg a:hover{
-   text-decoration:none;
-}
-
-//--></style>
-
-
-<script type="text/javascript"><!--
-function dsp(loc){
-   if(document.getElementById){
-      foc=loc.parentNode.nextSibling.style?
-         loc.parentNode.nextSibling:
-         loc.parentNode.nextSibling.nextSibling;
-      foc.style.display=foc.style.display=='block'?'none':'block';}}
-
-//-->
-</script>
-"""
-print "<h1>Your Packages</h1>"
-print "<ul>"
-for p in mList:
-    print '<div id="mypkg">'
-    if p in pathDict:
-        print """<li><a href=
-            "javascript:TextMate.system('\\'%s/bin/viewDoc.sh\\' %s', null);" >
-            %s</a></li>
-            """ % (os.environ["TM_BUNDLE_SUPPORT"], pathDict[p], descDict[p])
+    if (exists(docdbfile) and os.path.getmtime(docdbfile) > ninty_days_ago):
+        infile = open(docdbfile, 'rb')
+        path_desc_list = cPickle.load(infile)
+        pathDict = path_desc_list[0]
+        descDict = path_desc_list[1]
+        headings = path_desc_list[2]
+        cachedIndex = True
     else:
-        print """<li>%s</li>""" % (p)
-    print '</div>'
-print "</ul>"
+        # Part 2
+        # Parse the texdoctk database of packages
+        texMFbase = os.environ["TM_LATEX_DOCBASE"]
+        docIndex = os.environ["TEXDOCTKDB"]
 
-print "<hr />"
-print "<h1>Packages Browser</h1>"
-print "<ul>"
-for h in headings:
-    print('<li><a href="javascript:dsp(this)" class="dsphead" ' +
-          'onclick="dsp(this)">{}</a></li>'.format(h))
-    print '<div class="dspcont">'
+        docBase = texMFbase + "/"  # + "doc/"
+        if docBase[-5:].rfind('doc') < 0:
+            docBase = docBase + "doc/"
+
+        catalogDir = os.environ["TM_LATEX_HELP_CATALOG"]
+
+        texdocs = os.environ["TMTEXDOCDIRS"].split(':')
+        myDict = {}
+        for p in texdocs:
+            key = p[p.rfind('/')+1:]
+            myDict[key] = p
+
+        docDict = makeDocList()
+
+        try:
+            docIndexFile = open(docIndex, 'r')
+        except:
+            docIndexFile = []
+        for line in docIndexFile:
+            if line[0] == "#":
+                continue
+            elif line[0] == "@":
+                currentHeading = line[1:].strip()
+                headings[currentHeading] = []
+            else:
+                try:
+                    lineFields = line.split(';')
+                    key = lineFields[0]
+                    desc = lineFields[1]
+                    path = lineFields[2]
+                except:
+                    print "Error parsing line: ", line
+
+                headings[currentHeading].append(key)
+                if path.rfind('.sty') >= 0:
+                    path = docBase + "tex/" + path
+                else:
+                    path = docBase + path
+                    if not os.path.exists(path):
+                        # sometimes texdoctk.dat is misleading...
+                        altkey = path[path.rfind("/")+1:path.rfind(".")]
+                        if key in docDict:
+                            path = docDict[key]
+                        elif altkey in docDict:
+                            path = docDict[altkey]
+                        else:
+                            if key in myDict:
+                                path = findBestDoc(myDict[key])
+
+                pathDict[key] = path.strip()
+                descDict[key] = desc.strip()
+
+        # Part 3
+        # supplement texdoctk index with the regular texdoc catalog
+        try:
+            catList = os.listdir(catalogDir)
+        except:
+            catList = []
+        for fname in catList:
+            key = fname[:fname.rfind('.html')]
+            if key not in pathDict:
+                pathDict[key] = catalogDir + '/' + fname
+                descDict[key] = key
+                if key in docDict:
+                    pathDict[key] = docDict[key]
+
+        # Continue to supplement with searched for files
+        for p in docDict.keys()+myDict.keys():
+            if p not in pathDict:
+                if p in docDict:
+                    pathDict[p] = docDict[p].strip()
+                    descDict[p] = p
+                else:
+                    if p in myDict:
+                        path = findBestDoc(myDict[p])
+                        pathDict[p] = path.strip()
+                        descDict[p] = p
+
+        try:
+            if not os.path.exists(docdbpath):
+                os.mkdir(docdbpath)
+            outfile = open(docdbfile, 'wb')
+            cPickle.dump([pathDict, descDict, headings], outfile)
+        except:
+            print "<p>Error: Could not cache documentation index</p>"
+
+    # Part 4
+    # if a word was selected then view the documentation for that word
+    # using the best available version of the doc as determined above
+    cwPackage = os.environ.get("TM_CURRENT_WORD", None)
+    if cwPackage in pathDict:
+        os.system("viewDoc.sh " + pathDict[cwPackage])
+        sys.exit()
+
+    # Part 5
+    # Print out the results in html/javascript
+    # The java script gives us the nifty expand collapse outline look
+    print """
+    <style type="text/css"><!--
+    .save{
+       behavior:url(#default#savehistory);}
+    a.dsphead{
+       text-decoration:none;
+       font-family: "Lucida Grand", sans-serif
+       font-size: 120%;
+       font-weight: bold;
+       margin-left:0.5em;}
+    a.dsphead:hover{
+       text-decoration:underline;}
+    .dspcont{
+       display:none;
+       text-decoration:none;
+       font-family: "Bitstream Vera Sans Mono", "Monaco", monospace;
+       margin: 0px 20px 0px 20px;}
+    .dspcont a{
+        text-decoration: none;
+    }
+    .dspcont a:hover{
+        text-decoration:underline;
+    }
+    div#mypkg{
+       text-decoration:none;
+       font-family: "Bitstream Vera Sans Mono", "Monaco", monospace;
+    }
+    div#mypkg a{
+       text-decoration:none;
+       font-family: "Bitstream Vera Sans Mono", "Monaco", monospace;
+    }
+    div#mypkg a:hover{
+       text-decoration:none;
+    }
+
+    //--></style>
+
+
+    <script type="text/javascript"><!--
+    function dsp(loc){
+       if(document.getElementById){
+          foc=loc.parentNode.nextSibling.style?
+             loc.parentNode.nextSibling:
+             loc.parentNode.nextSibling.nextSibling;
+          foc.style.display=foc.style.display=='block'?'none':'block';}}
+
+    //-->
+    </script>
+    """
+    print "<h1>Your Packages</h1>"
     print "<ul>"
-    for p in headings[h]:
-        if os.path.exists(pathDict[p]):
-            print(
-                """<li><a href=
-                  "javascript:TextMate.system(
-                      '\\'%s/bin/viewDoc.sh\\' %s', null);">
-                  %s</a></li>""" % (os.environ["TM_BUNDLE_SUPPORT"],
-                                    pathDict[p], descDict[p]))
+    for p in mList:
+        print '<div id="mypkg">'
+        if p in pathDict:
+            print("""<li><a href= "javascript:
+                     TextMate.system('\\'%s/bin/viewDoc.sh\\' %s', null);">
+                     %s</a></li>
+                  """ % (os.environ["TM_BUNDLE_SUPPORT"], pathDict[p],
+                         descDict[p]))
         else:
             print """<li>%s</li>""" % (p)
+        print '</div>'
     print "</ul>"
-    print '</div>'
-print "</ul>"
-if cachedIndex:
-    print("""<p>You are using a saved version of the LaTeX documentation
-             index.  This index is automatically updated every 90 days. If you
-             want to force an update simply remove the file %s </p>
-          """ % docdbfile)
+
+    print "<hr />"
+    print "<h1>Packages Browser</h1>"
+    print "<ul>"
+    for h in headings:
+        print('<li><a href="javascript:dsp(this)" class="dsphead" ' +
+              'onclick="dsp(this)">{}</a></li>'.format(h))
+        print '<div class="dspcont">'
+        print "<ul>"
+        for p in headings[h]:
+            if os.path.exists(pathDict[p]):
+                print("""<li><a href="javascript:TextMate.system(""" +
+                      """'\\'{}/bin/viewDoc.sh\\' """.format(
+                          os.environ["TM_BUNDLE_SUPPORT"]) +
+                      """{}', null);"> {}</a></li>""".format(
+                          pathDict[p], descDict[p]))
+            else:
+                print """<li>%s</li>""" % (p)
+        print "</ul>"
+        print '</div>'
+    print "</ul>"
+    if cachedIndex:
+        print("""<p>You are using a saved version of the LaTeX documentation
+                 index. This index is automatically updated every 90 days. If
+                 you want to force an update simply remove the file %s </p>
+              """ % docdbfile)
