9c6b722186f35794150c7472e24cc59298bd7352
René Schwaiger <sanssecours@f-m.fm>
2015-03-15 22:33:40 +0100

    Simplify script for label completion

--- Support/bin/label_completion.rb
+++ Support/bin/label_completion.rb
@@ -1,8 +1,9 @@
-#!/usr/bin/env ruby18
-require ENV["TM_BUNDLE_SUPPORT"] + "/lib/latex.rb"
-phrase = ENV['TM_CURRENT_WORD']
-include LaTeX
-items = LaTeX.labels
-items = items.grep(/^#{Regexp.escape(phrase)}/) if phrase != ""
-exit if items.empty?
-puts items.join("\n")
+#!/usr/bin/ruby
+
+# -- Imports -------------------------------------------------------------------
+
+require ENV['TM_BUNDLE_SUPPORT'] + '/lib/latex.rb'
+
+# -- Main ----------------------------------------------------------------------
+
+puts(LaTeX.labels.join("\n"))
