77e61b459e211a2cec114dfd1358107f384974cb
René Schwaiger <sanssecours@f-m.fm>
2015-03-18 10:24:45 +0100

    Refactor code for “Show Outline”
    #ignore

--- Support/lib/command.rb
+++ Support/lib/command.rb
@@ -339,95 +339,175 @@ end
 # = Show Outline =
 # ================
 
-class String
-  def adjust_end(new_piece)
-    master = LaTeX.master(ENV['TM_LATEX_MASTER'] || ENV['TM_FILEPATH'])
-    new_form = self
-    if master == self
-      new_form = sub(/[^\/]*$/, new_piece)
-    else
-      masterdir = File.dirname(master)
-      new_form = File.expand_path(new_piece, masterdir)
-    end
-    new_form += '.tex' unless new_form.match(/\.tex$/) || File.exist?(new_form)
-    new_form
-  end
-end
-
+# This class represent a section of a tex document.
+#
+# A section acts the same way as a strings. The class implements additional
+# methods that allows us to take the order of a section into account.
 class Section < String
-  def <=>(other)
-    parts = %w(root part chapter section subsection subsubsection paragraph
+  # rubocop:disable Style/ClassVars
+  @@parts = %w(part chapter section subsection subsubsection paragraph
                subparagraph)
-    parts.index(self) <=> parts.index(other)
+
+  # Get the number of levels from the current to the given section.
+  #
+  # The returned value specifies how many levels it takes to get from the
+  # current section to the given section.
+  #
+  # = Arguments
+  #
+  # [other] The other section that should be compared to this one.
+  #
+  # = Output
+  #
+  # The function returns a number specifying how many sections down (positive
+  # value), or up (negative value) it takes to get to the given section.
+  #
+  #  doctest: The section 'section' is two levels down starting from 'part'
+  #  >> Section.new('part').levels_to('section')
+  #  => 2
+  #
+  #  doctest: The section 'part' is two levels up starting from 'paragraph'
+  #  >> Section.new('paragraph').levels_to('part')
+  #  => -5
+  def levels_to(other)
+    @@parts.index(other) - @@parts.index(self)
   end
 
-  def <(other)
-    (self <=> other) == -1
+  # Get the section with the smallest level in a list of strings.
+  #
+  # = Arguments
+  #
+  # [part_list] A list containing section strings.
+  #
+  # = Output
+  #
+  # The function returns a new section.
+  #
+  # = Examples
+  #
+  #  doctest: Get the smallest section from a list of strings.
+  #  >> Section.smallest_part(['paragraph', 'section', 'part', 'subsection'])
+  #  => 'part'
+  def self.smallest_part(part_list)
+    Section.new @@parts[part_list.map { |part| @@parts.index(part) }.min]
   end
 end
 
-def outline_points(filename)
-  part = '(part|chapter|section|subsection|subsubsection|paragraph|' \
+# Various code used to show an outline — table of contents — for a tex document.
+module Outline
+  # Get an HTML outline from the given tex file.
+  #
+  # = Arguments
+  #
+  # [filepath] The path to the file for which we want to get an HTML outline.
+  #
+  # = Output
+  #
+  # The function returns a string containing HTML lists that reference the
+  # different part of the given document.
+  def self.outline_from_file(filepath)
+    outline_points_to_html(outline_points(filepath))
+  end
+
+  private
+
+  PART = '(part|chapter|section|subsection|subsubsection|paragraph|' \
          'subparagraph)\*?'
-  comment = '(?:%.*\n[ \t]*)?'
-  options = '(?>\[(.*?)\])'
-  argument = '\{([^{}]*(?:\{[^}]*\}[^}]*?)*)\}'
-  regex = /\\#{part}#{comment}(?:#{options}|#{argument})/
-  include_regex = /\\(?:input|include)(?:%.*\n[ \t]*)?(?>\{(.*?)\})/
-  non_comment_regex = /^([^%]+$|(?:[^%]|\\%)*)(?=%|$)/
-  points = []
-  if filename.is_a?(String)
-    data = File.read(filename)
-    name = 'url=file://' + e_url(filename) + '&'
-  else
-    data = filename.read
-    name = ''
+  COMMENT = '(?:%.*\n[ \t]*)?'
+  OPTIONS = '(?>\[(.*?)\])'
+  ARGUMENT = '\{([^{}]*(?:\{[^}]*\}[^}]*?)*)\}'
+
+  PART_REGEX = /\\#{PART}#{COMMENT}(?:#{OPTIONS}|#{ARGUMENT})/
+  INCLUDE_REGEX = /\\(?:input|include)#{COMMENT}(?>\{(.*?)\})/
+  NON_COMMENT_REGEX = /^([^%]+$|(?:[^%]|\\%)+)(?=%|$)/
+
+  class <<self
+    # Get the text stored in and a URL referencing +filename+.
+    def content_url(filename)
+      if filename.is_a?(String)
+        [File.read(filename), "url=file://#{e_url(filename)}&"]
+      else
+        [filename.read, '']
+      end
+    end
+
+    # Try to get a outline point — containing url, line number, section and
+    # the text of the section — from a single line of text.
+    def outline_point_from_line(line, url, linenumber)
+      [url, linenumber, Regexp.last_match[1], Regexp.last_match[2] ||
+        Regexp.last_match[3]] if line.match(PART_REGEX)
+    end
+
+    # Try to get outline points from a file referenced in a line of text.
+    def outline_points_from_line(line)
+      if line.match(INCLUDE_REGEX)
+        outline_points(join_with_master_path(Regexp.last_match[1]))
+      else
+        []
+      end
+    end
+
+    # Get all the outline points contained in +filename+.
+    def outline_points(filename)
+      data, url = content_url(filename)
+      points = []
+      data.split("\n").each_with_index do |line, linenumber|
+        next unless line.match(NON_COMMENT_REGEX)
+        points << outline_point_from_line(Regexp.last_match[1], url,
+                                          linenumber + 1)
+        points += outline_points_from_line(line)
+      end
+      points.compact
+    end
+
+    # Convert a list of outline points to HTML.
+    def outline_points_to_html(points)
+      smallest_part = Section.smallest_part(points.map { |_, _, part, _| part })
+      last_part = smallest_part
+      items = points.map do |filepath, line, part, title|
+        levels = last_part.levels_to(part)
+        last_part = Section.new(part)
+        "#{(levels > 0) ? '<ol>' * levels : '</ol>' * levels.abs}<li>
+          <a href=\"txmt://open?#{filepath}line=#{line}\">#{title}</a></li>"
+      end
+      "<ol>#{items.join("\n")}</ol>" +
+        '</ol>' * last_part.levels_to(smallest_part).abs
+    end
   end
-  data.split("\n").each_with_index do |line, linenumber|
-    line.match(non_comment_regex)
-    line = Regexp.last_match[1]
-    next unless line
-    points << [
-      name, linenumber + 1, Regexp.last_match[1],
-      Regexp.last_match[2] || Regexp.last_match[3]] if line.match(regex)
-    points += outline_points(
-      filename.adjust_end(Regexp.last_match[1])) if line.match(include_regex)
-  end
-  points
 end
 
+# Add the given path to the path of the current master file.
+#
+# = Arguments
+#
+# [filepath] The path that should be added to the master path
+#
+# = Output
+#
+# The function returns a string containing a filepath.
+#
+# = Example
+#
+#  doctest: Add the path 'input/references_input' to the current master path
+#  >> ENV['TM_FILEPATH'] = 'Tests/TeX/references.tex'
+#  >> join_with_master_path('input/references_input').end_with? (
+#     'Tests/TeX/input/references_input.tex')
+#  => true
+def join_with_master_path(filepath)
+  master = LaTeX.master(ENV['TM_LATEX_MASTER'] || ENV['TM_FILEPATH'])
+  path = (master == self) ? sub(/[^\/]*$/, filepath) :
+    File.expand_path(filepath, File.dirname(master))
+  "#{path}#{'.tex' unless path.match(/\.tex$/) || File.exist?(path)}"
+end
+
+# Show an outline for the current tex document.
+#
+# The function shows a clickable HTML view for the current tex document.
 def show_outline
   html_header 'LaTeX Document Outline', 'LaTeX'
   file = LaTeX.master(ENV['TM_LATEX_MASTER'] || ENV['TM_FILEPATH'])
-  if file.nil?
-    file = STDIN
-  else
-    file = File.expand_path(file, File.dirname(ENV['TM_FILEPATH']))
-  end
-  require 'pp'
-  points = outline_points(file)
-  current = [Section.new('root')]
-  string = []
-  points.each do |filepath, line, part, title|
-    case current.last <=> part
-    when 1
-      loop do
-        current.pop
-        string << "\t" * current.length + '</ol>'
-        break if current.last > part
-      end
-    when -1
-      string << "\t" * current.length + '<ol>'
-      current << Section.new(part)
-    end
-    string << "\t" * current.length + (
-      "<li><a href=\"txmt://open?#{filepath}line=#{line}\">#{title}</a></li>")
-  end
-  loop do
-    current.pop
-    string << "\t" * current.length + '</ol>'
-    break if current.last != 'root'
-  end
-  puts string.join("\n")
+  file = file.nil? ? STDIN :
+                     File.expand_path(file, File.dirname(ENV['TM_FILEPATH']))
+  puts(Outline.outline_from_file(file))
   html_footer
 end
