f185ba6ce90ce95bcba48403300aded4742f2cbf
René Schwaiger <sanssecours@f-m.fm>
2015-05-24 17:13:37 +0200

    Add function `tex_directives`
    This function parses the tex directives of a certain file. It returns a
    hash containing all found directives.

--- Support/lib/Perl/Latex.pm
+++ Support/lib/Perl/Latex.pm
@@ -1,58 +1,85 @@
 package Latex;
 
+# -- Imports -------------------------------------------------------------------
+
 use strict;
 use warnings;
 
 use Carp qw( croak );
 use Exporter qw(import);
 
-our @EXPORT_OK = qw(guess_tex_engine);
+# -- Exports -------------------------------------------------------------------
+
+our @EXPORT_OK = qw(guess_tex_engine tex_directives);
+
+# -- Functions -----------------------------------------------------------------
 
 # Guess the TeX engine which should be used to translate a certain TeX-file.
 #
 # Arguments:
 #
-#      filepath - The file path to the TeX file either as absolute path or
-#                 relative to the location of this file
+#      filepath - The file path of the TeX file.
 #
 # Returns:
 #
 #      A string containing the TeX engine for the given file or an empty
-#      string if the engine could not be determined
-#
-# Example:
-#
-#   We assume `test.tex` contains the line `%!TEX TS-program = pdflatex`
-#   $ guess_tex_engine(test.tex)
-#   "pdflatex"
+#      string if the engine could not be determined.
 #
 sub guess_tex_engine {
     my ($filename) = @_;
-    open( my $fh, "<", $filename )
-      or croak "Can not open $filename: $!";
-    my $engine = _get_engine_from_file($fh);
-    close($fh);
+    my $engine     = "";
+    my %directives = tex_directives($filename);
+
+    $engine = $directives{"program"} if ( exists $directives{"program"} );
     return $engine;
 }
 
-sub _get_engine_from_file {
+# Read `%! TEX` directives from a given file.
+#
+# Arguments:
+#
+#      filepath - The file path of the TeX file.
+#
+# Returns:
+#
+#      A hash containing the tex directives for the given file.
+#
+sub tex_directives {
+    my ($filename) = @_;
+    open( my $fh, "<", $filename )
+      or croak "Can not open $filename: $!";
+    my %directives = _tex_directives_filehandle($fh);
+    close($fh);
+    return %directives;
+}
+
+# ===========
+# = Private =
+# ===========
+
+sub _tex_directives_filehandle {
     my ($fh) = @_;
-    my $engine = "";
+    my %directives = ();
 
     # TS-program is case insensitive e.g. `LaTeX` should be the same as `latex`
     my $engines = "(?i)latex|lualatex|pdflatex|xelatex(?-i)";
+    my $keys    = "encoding|spellcheck|root";
 
     while ( my $line = <$fh> ) {
         last unless ( 1 .. 20 );
-        if ( $line =~
-            m{%!TEX (?:\s+) (?:TS-)program (?:\s*)=(?:\s*) ($engines)}x )
-        {
-            $engine = lc($1);
-            last;
+        next unless ( $line =~ m{^ \s*%\s* !TEX}x );
+
+        $line =~ s/^ \s*%\s* !TEX \s* | \s+$//x;
+
+        if ( $line =~ m{(?:TS-)program (?:\s*)=(?:\s*) ($engines)}x ) {
+            $directives{"program"} = lc($1);
+        }
+        elsif ( $line =~ m{($keys) (?:\s*)=(?:\s*) (.+)}x ) {
+            $directives{$1} = $2;
         }
     }
-    return $engine
 
+    return %directives;
 }
 
 1;
--- Tests/Perl/latex.t
+++ Tests/Perl/latex.t
@@ -1,16 +1,21 @@
 #!/usr/bin/perl
 
+# -- Imports -------------------------------------------------------------------
+
 use strict;
 use warnings;
 
 use Cwd qw(abs_path);
 use File::Basename;
-use Test::Simple tests => 3;
+use Test::More tests => 5;
 
 use lib dirname( dirname( dirname abs_path $0) ) . '/Support/lib/Perl';
-use Latex 'guess_tex_engine';
+use Latex qw(guess_tex_engine tex_directives);
+
+# -- Tests ---------------------------------------------------------------------
 
 my $tex_dir = dirname( dirname abs_path $0) . '/TeX';
+my ( %reference, %output );
 
 ok( guess_tex_engine( $tex_dir . '/xelatex.tex' ) eq 'xelatex',
     'Guess tex engine for xelatex.tex' );
@@ -18,3 +23,18 @@ ok( guess_tex_engine( $tex_dir . '/ünicöde.tex' ) eq 'xelatex',
     'Guess tex engine for ünicöde.tex' );
 ok( guess_tex_engine( $tex_dir . '/text.tex' ) eq '',
     'Guess tex engine for text.tex' );
+
+%reference = (
+    'encoding'   => 'UTF-8 Unicode',
+    'program'    => 'xelatex',
+    'spellcheck' => 'en-US'
+);
+%output = tex_directives( $tex_dir . '/xelatex.tex' );
+
+is_deeply( \%reference, \%output, 'Check tex directives for file xelatex.tex' );
+
+%reference = ( 'root' => './packages_input2.tex' );
+%output = tex_directives( $tex_dir . '/input/packages_input1.tex' );
+
+is_deeply( \%reference, \%output,
+    'Check tex directives for file packages_input1.tex' );
