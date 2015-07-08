1ed061cc48d547d28b14517870fa4365108c6052
René Schwaiger <sanssecours@f-m.fm>
2015-07-08 06:55:47 +0200

    Rename `tex_prefs` to `preferences`
    #ignore

--- Support/bin/configure.py
+++ Support/bin/configure.py
@@ -15,5 +15,5 @@ from tmprefs import Preferences
 if __name__ == '__main__':
     prefs = Preferences()
     command = ('"$DIALOG" -mp "" -d \'{}\' '.format(prefs.defaults()) +
-               '"$TM_BUNDLE_SUPPORT"/nibs/tex_prefs.nib')
+               '"$TM_BUNDLE_SUPPORT"/nibs/preferences.nib')
     Popen(command, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
