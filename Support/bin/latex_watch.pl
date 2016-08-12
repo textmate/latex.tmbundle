10b1fa94bff6f044e58917a4270979e07309ee2b
René Schwaiger <sanssecours@f-m.fm>
2016-08-12 18:40:26 +0200

    “Watch Document”: Remove bundle cache file
    After this change “Watch Document” also removes the bundle cache file
    again (`.filename.lb`), after we close the preview program.

--- Support/bin/latex_watch.pl
+++ Support/bin/latex_watch.pl
@@ -286,6 +286,8 @@ sub main_loop {
 sub clean_up {
     debug_msg("Cleaning up");
     fail_unless_system( "clean.rb", "$filepath" );
+    # Remove LaTeX bundle cache file
+    unlink("$absolute_wd/\.$name.lb");
     $cleanup_viewer->() if defined $cleanup_viewer;
     if ( defined($progressbar_pid) ) {
         debug_msg("Closing progress bar window as part of cleanup");
@@ -1113,3 +1115,7 @@ Changes
 
 3.1415:
     - Use `clean.rb` to remove auxiliary files
+
+3.14159:
+    - The command now also removes the bundle cache file (`.filename.lb`)
+      again, after we close the preview program.
