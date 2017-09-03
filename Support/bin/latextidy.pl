e2c95e734bd07d9bf81bfc7c8525691869ef660a
heptadecagon <6902039+heptadecagon@users.noreply.github.com>
2017-09-03 13:25:07 -0400

    Tidy: Add new lines after \cr
    Add new lines after "\cr" in addition to "\\" and "\\[...]".

--- Support/bin/latextidy.pl
+++ Support/bin/latextidy.pl
@@ -108,12 +108,12 @@ foreach (@pieces){
 	s/(\\item)/\n$1/g;
 
 #\n before each \[  and after each \]
-#Add newlines after all "\\" and "\\[...]"
+#Add newlines after all "\\", "\cr", and "\\[...]"
 
 	s/[^\\](\\\[)/\n$1/g;
 	s/(\\\])/$1\n/g;
 
-	s/(\\\\)\s/$1\n/g;
+	s/(\\\\|\\cr)\s/$1\n/g;
 	s/(\\\\\[)(.*?)(\])\s/$1$2$3\n/g;
 
 
