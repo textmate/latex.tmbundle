4048e101d81c0f12992fcca6ae6cb6e5d5b8736f
René Schwaiger <sanssecours@me.com>
2018-03-29 17:24:53 +0200

    Typeset & View: Add location of `ps2pdf` to `PATH`
    Translating a document using the engine `latex` should now also work,
    if the default location of `ps2pdf` is not part of `PATH`.

--- Support/bin/texmate.py
+++ Support/bin/texmate.py
@@ -1055,6 +1055,8 @@ if __name__ == '__main__':
         if engine == 'latex':
             call("dvips {0}.dvi -o '{0}.ps'".format(file_without_suffix),
                  shell=True)
+            # Add default location of `ps2pdf` to `PATH`
+            putenv('PATH', getenv('PATH') + ':/usr/local/bin')
             call("ps2pdf '{}.ps'".format(file_without_suffix), shell=True)
         if tm_autoview and number_errors < 1 and not suppress_viewer:
             viewer_status = run_viewer(
